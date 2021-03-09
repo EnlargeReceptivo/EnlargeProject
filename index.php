750bf5f57b01bc6c692811abbbaa1f27d7a139ae
Lobo <Lobo@LOBO.lan>
2021-03-09 17:53:40 -0300

    Mudei o title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Enlarge - project</title>
     </head>
     <body>
         <?php
